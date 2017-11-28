d6c432adb75d19fb75024dea499498b9b7dfe387
maxjacobson <max.jacobson@torchlms.com>
2017-11-28 00:04:14 -0700

    remove old server files

--- src/server/config/database.js
+++
@@ -1,7 +0,0 @@
-var Sequelize = require('sequelize');
-var secrets = require('./secrets');
-var sequelize = new Sequelize(secrets.databaseConfig, {
-    operatorsAliases: false
-});
-
-module.exports.database = sequelize;
