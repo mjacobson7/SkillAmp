bb9ffcb3b49590d315e80981635734f6ec0db960
maxjacobson <max.jacobson@torchlms.com>
2018-01-14 18:02:41 -0700

    improve style of dashboard and header

--- src/app/components/leaderboard/leaderboard.component.ts
+++ src/app/components/leaderboard/leaderboard.component.ts
@@ -9,10 +9,18 @@ import {MatPaginator, MatTableDataSource} from '@angular/material';
   styleUrls: ['./leaderboard.component.css']
 })
 export class LeaderboardComponent implements OnInit {
-  displayedColumns = ['position', 'name', 'weight'];
+  displayedColumns = ['rank', 'name', 'score', 'supervisor'];
   dataSource = new MatTableDataSource<Element>(ELEMENT_DATA);
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
+
+
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+  }
+
 
   constructor() { }
   
@@ -32,31 +40,31 @@ export class LeaderboardComponent implements OnInit {
 
 export interface Element {
   name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  rank: number;
+  score: number;
+  supervisor: string;
 }
 
 const ELEMENT_DATA: Element[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
-  {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
-  {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
-  {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
-  {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
-  {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
-  {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
-  {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
-  {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
-  {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
+  {rank: 1, name: 'Ryan Gray', score: 5.00, supervisor: 'Jim Carter'},
+  {rank: 2, name: 'Max Jacobson', score: 4.95, supervisor: 'Jim Carter'},
+  {rank: 3, name: 'Sushant Shrestha', score: 4.90, supervisor: 'Jim Carter'},
+  {rank: 4, name: 'Paul Mefford', score: 4.75, supervisor: 'Jim Carter'},
+  {rank: 5, name: 'Matt Behrend', score:4.60, supervisor: 'Jim Carter'},
+  {rank: 6, name: 'Lea Burr', score: 4.30, supervisor: 'Jim Carter'},
+  {rank: 7, name: 'Jordan Woodhouse', score: 4.20, supervisor: 'Jim Carter'},
+  {rank: 8, name: 'Jaron Mathis', score: 4.00, supervisor: 'Jim Carter'},
+  {rank: 9, name: 'Jake White', score: 3.95, supervisor: 'Jim Carter'},
+  {rank: 10, name: 'Chris Duran', score: 3.80, supervisor: 'Jim Carter'},
+  {rank: 11, name: 'Bethany Woodhouse', score: 3.5, supervisor: 'Jim Carter'},
+  {rank: 12, name: 'Spencer Jacox', score: 3.25, supervisor: 'Jim Carter'},
+  {rank: 13, name: 'Carly Warner', score: 3.15, supervisor: 'Jim Carter'},
+  {rank: 14, name: 'Brandon Howell', score: 3.00, supervisor: 'Jim Carter'},
+  {rank: 15, name: 'Brian Haney', score: 2.80, supervisor: 'Jim Carter'},
+  {rank: 16, name: 'Eric Peterson', score: 2.80, supervisor: 'Jim Carter'},
+  {rank: 17, name: 'Joey Eva', score: 2.60, supervisor: 'Jim Carter'},
+  {rank: 18, name: 'James Mitchell', score: 2.45, supervisor: 'Jim Carter'},
+  {rank: 19, name: 'Chris Johanson', score: 2.05, supervisor: 'Jim Carter'},
+  {rank: 20, name: 'Alex Fredrickson', score: 1.25, supervisor: 'Jim Carter'},
 ];
 
