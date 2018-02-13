404d9f5f4a6a07661b439fa36ebeb4ffc9d9bdb1
maxjacobson <max.jacobson@torchlms.com>
2018-02-13 13:58:21 -0700

    Implement unsubscribe for all subscriptions currently active on site

--- src/app/components/add-edit-user/add-edit-user.component.ts
+++ src/app/components/add-edit-user/add-edit-user.component.ts
@@ -1,29 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-add-edit-user',
   templateUrl: './add-edit-user.component.html',
   styleUrls: ['./add-edit-user.component.css']
 })
-export class AddEditUserComponent implements OnInit {
+export class AddEditUserComponent implements OnInit, OnDestroy {
   user;
-  // TODO: Make pageInfo title dynamic so it says 'Create New User' or 'Edit User'  
+  userIdSubscription: Subscription;
+  // TODO: Make pageInfo title dynamic so it says 'Create New User' or 'Edit User'
   pageInfo: {title: string, icon: string} = {
-    title: 'User', 
+    title: 'User',
     icon: 'person'
-  }
+  };
   username;
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.user = {
-      id: this.route.snapshot.paramMap.get('id')      
-    }
-    this.route.params.subscribe(params => {
-      this.user.id = params['id']
-    })
+      id: this.route.snapshot.paramMap.get('id')
+    };
+    this.userIdSubscription = this.route.params.subscribe(params => {
+      this.user.id = params['id'];
+    });
+  }
+
+  ngOnDestroy() {
+    this.userIdSubscription.unsubscribe();
   }
 
 
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,32 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AppError } from '../../common/app-error';
 import { BadInput } from '../../common/bad-input';
 import { AuthService } from '../../services/auth/auth.service';
 import { Router } from '@angular/router';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
-  invalidLogin: boolean = false;
-
+export class LoginComponent implements OnInit, OnDestroy {
+  invalidLogin = false;
+  loginSubscription: Subscription;
   constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
   }
 
   login(credentials: NgForm) {
-    this.authService.login(credentials)
+    this.loginSubscription = this.authService.login(credentials)
       .subscribe(result => {
-        if(result) {
+        if (result) {
           this.router.navigate(['/dashboard']);
           this.invalidLogin = false;
-        }
-        else this.invalidLogin = true;
-      })
+        } else { this.invalidLogin = true; }
+      });
+    }
+
+    ngOnDestroy() {
+      this.loginSubscription.unsubscribe();
     }
 
 
