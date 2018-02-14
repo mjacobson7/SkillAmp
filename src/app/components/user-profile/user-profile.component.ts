c14c827ceb55ebe3cff8f36a2211eeda992a25f2
maxjacobson <max.jacobson@torchlms.com>
2018-02-14 00:09:31 -0700

    Add form validation for user profile

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import {FormGroup, FormControl, Validators, FormBuilder, FormArray} from '@angular/forms';
 import { AuthService } from '../../services/auth/auth.service';
 import { UserService } from '../../services/user/user.service';
 import {NavService} from '../../services/nav/nav.service';
@@ -13,13 +13,15 @@ import {Subscription} from 'rxjs/Subscription';
 export class UserProfileComponent implements OnInit, OnDestroy {
   user;
   userForm: FormGroup;
+  passwordLabel = ['Password', 'Confirm password'];
   userFormSubscription: Subscription;
+  changePassword: Boolean;
   pageInfo: {title: string, icon: string} = {
     title: 'My Profile',
     icon: 'person'
   };
 
-  constructor(private authService: AuthService, private userService: UserService, private navService: NavService) { }
+  constructor(private authService: AuthService, private userService: UserService, private navService: NavService, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.navService.pageHeaderTitle.next(this.pageInfo);
@@ -27,27 +29,43 @@ export class UserProfileComponent implements OnInit, OnDestroy {
     this.userFormSubscription = this.authService.user.subscribe(response => {
       this.user = response;
 
-      this.userForm = new FormGroup({
-        'username': new FormControl(this.user.username),
-        'firstName': new FormControl(this.user.firstName),
-        'lastName': new FormControl(this.user.lastName),
-        'email': new FormControl(this.user.email),
-        'supervisor': new FormControl(this.user.supervisor),
-        'role': new FormControl(this.user.role),
+      this.userForm = this.formBuilder.group({
+        'username': new FormControl(this.user.username, [Validators.required, Validators.minLength(8)]),
+        'firstName': new FormControl(this.user.firstName, Validators.required),
+        passwords: this.formBuilder.array([
+          ['', Validators.compose([Validators.required, Validators.minLength(8)])],
+          ['', Validators.compose([Validators.required, Validators.minLength(8)])]
+        ], this.passwordValidator),
+        'lastName': new FormControl(this.user.lastName, Validators.required),
+        'email': new FormControl(this.user.email, [Validators.required, Validators.email]),
+        'supervisor': new FormControl(this.user.supervisor, Validators.required),
+        'role': new FormControl(this.user.role, Validators.required),
       });
-
-
     });
+  }
 
+  passwordValidator(array: FormArray): {[s: string]: boolean} {
+    return array.value[0] === array.value[1] ? null : {'unmatched': true};
+  }
 
-
+  getControls() {
+    return (<FormArray>this.userForm.get('passwords')).controls;
   }
 
   onSubmit() {
+    console.log(this.userForm);
     this.userService.updateProfile(this.userForm.value)
       .subscribe(response => {
         this.user = response;
     });
+  }
+
+  changePasswordChecked(event) {
+    if (event.target.checked) {
+      this.changePassword = true;
+    } else {
+      this.changePassword = false;
+    }
   }
 
   ngOnDestroy() {
