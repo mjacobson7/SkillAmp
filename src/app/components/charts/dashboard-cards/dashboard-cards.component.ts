f1517b6945127307d5d72266a0700e6b0a4ee093
maxjacobson <max.jacobson@torchlms.com>
2018-04-06 14:39:04 -0600

    update all cards on dashboard to the same design

--- src/app/components/charts/dashboard-cards/dashboard-cards.component.ts
+++ src/app/components/charts/dashboard-cards/dashboard-cards.component.ts
@@ -8,15 +8,6 @@ import { DashboardService } from '../../../services/dashboard/dashboard.service'
 })
 export class DashboardCardsComponent implements OnInit {
 
-
-
-  // totalAgentReviews = [{name: "Total Reviews", value: 37}];
-  // averageAgentScore = [{name: "Average Agent Score", value: 3.57}] ;
-  // averageTeamScore = [{name: "Average Team Score", value: 4.25}];
-  // averageCompanyScore = [{name: "Average Company Score", value: 3.35}];
-  // agentRankForTeam = [{name: "Agent Team Rank", value: 4}];
-  // agentRankForCompany = [{name: "Agent Company Rank", value: 12}];
-
   constructor(private dashboardService: DashboardService) {
   }
 
